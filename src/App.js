e9b9d7f2ae66fd9198d99fd978130360706930ea
Vu Tran <tran.vuhoangsf19@gmail.com>
2024-09-23 19:00:52 +0700

    handle sign in

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import SignInPage from './perspective/SignInPage';
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/signin" element={<SignInPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
